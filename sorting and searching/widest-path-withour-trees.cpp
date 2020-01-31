5c048847c2757ccd6192e16eabd6eadd1c47b56f
Prashanth Rao <psprao95@gmail.com>
2020-01-31 15:51:35 -0600

    widest path without trees

---
+++ sorting and searching/widest-path-withour-trees.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minPathSum(vector<int>& X,vector<int> &Y) {
+        sort(X.begin(),X.end());
+        int n=X.size()
+        int res=0;
+        for(int i=1;i<n;i++)
+        {
+          res=max(res,X[i]-X[i-1]);
+        }
+
+        return res;
+    }
+
+
+};
